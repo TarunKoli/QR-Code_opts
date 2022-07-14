9047b37b44c840b59d7b4538f62e6e7f23781d2e
TarunKoli <amankumar8348@gmail.com>
2022-07-14 23:04:59 +0530

    Added Download Qr functionality

--- QR_Generator/script.js
+++ QR_Generator/script.js
@@ -1,18 +1,41 @@
 const wrapper = document.querySelector(".wrapper");
-const generateBtn = wrapper.querySelector(".form button");
+const generateBtn = wrapper.querySelector(".btns .generate");
 const qrInput = wrapper.querySelector(".form input");
 const qrImg = wrapper.querySelector(".qr-code img");
+const downloadBtn = wrapper.querySelector(".download");
 
 generateBtn.addEventListener("click", () => {
   let qrValue = qrInput.value;
   if (!qrValue) return;
+
   generateBtn.innerText = "Generating QR Code....";
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  const QR = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  qrImg.src = QR;
+
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR Code";
+
+    downloadBtn.addEventListener("click", () => {
+      download(QR);
+    });
   });
 });
+
+function download(QR) {
+  fetch(QR)
+    .then((res) => res.blob())
+    .then((file) => {
+      let tempUrl = URL.createObjectURL(file);
+      let aTag = document.createElement("a");
+      aTag.href = tempUrl;
+      aTag.download = "qr.png";
+      document.body.appendChild(aTag);
+      aTag.click();
+      URL.revokeObjectURL(file);
+      aTag.remove();
+    });
+}
 
 qrInput.addEventListener("keyup", () => {
   if (!qrInput.value) {
