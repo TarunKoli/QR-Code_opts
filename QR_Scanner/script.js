3e507ac5ad08f5183b62007e8543b1a24ed59cc2
TarunKoli <amankumar8348@gmail.com>
2022-07-14 23:36:56 +0530

    Ready to deploy fixed prod bug

--- QR_Scanner/script.js
+++ QR_Scanner/script.js
@@ -7,7 +7,7 @@ const closeBtn = wrapper.querySelector(".close");
 
 function fetchRequest(formData, file) {
   infoText.innerHTML = "Scanning QR Code....";
-  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+  fetch("https://api.qrserver.com/v1/read-qr-code/", {
     method: "POST",
     body: formData,
   })
